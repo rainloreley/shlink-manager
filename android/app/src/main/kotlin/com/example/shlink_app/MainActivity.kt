65853b256f009ee770f844f6fff7c3e8713b3159
Leó Takács <leo.takacs@yahoo.com>
2025-08-12 17:10:20 +0200

    Ran flutter create to make gradle versions up-to-date

---
+++ android/app/src/main/kotlin/com/example/shlink_app/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.shlink_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
--- android/app/src/main/kotlin/dev/abmgrt/shlink_app/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package dev.abmgrt.shlink_app
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
